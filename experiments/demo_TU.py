272684cfa160461f7ae2c1c420ae55cd1562fca0
ed2189 <ed2189@nyu.edu>
2025-05-06 21:07:41 -0400

    setting up HCP

--- experiments/demo_TU.py
+++ experiments/demo_TU.py
@@ -2,33 +2,35 @@ import torch
 from itu_auction import get_template
 import time
 # Example of using the TU template
-num_i = 1500
-num_j = 1300
+num_i = 600
+num_j = 500
 
-torch.manual_seed(125)
-Φ_i_j = (torch.randint(0, 9, (num_i, 1)) - torch.randint(0, 9, (1,num_j ))) **2
-market = get_template("TU")(Φ_i_j)
-market.method = "GS"
-market.sampling_rate = 0.1
 
-# Solve 
-eps = 1e-3
-scaling_factor = 0.5
-eps_init = 10
-tic = time.time()
-# u_i, v_j, mu_i_j = market.forward_auction(eps= eps, return_mu_i_j=True)
-# u_i, v_j, mu_i_j = market.reverse_auction(eps= eps, return_mu_i_j=True)
-u_i, v_j, mu_i_j = market.forward_reverse_scaling(eps_init, eps, scaling_factor)
-toc = time.time()
-print(f"Time taken for auction: {toc - tic:.4f} seconds")
+for _ in range(200):
+    torch.manual_seed(_)
+    Φ_i_j = (torch.randint(0, 9, (num_i, 1)) - torch.randint(0, 9, (1,num_j ))) **2
+    market = get_template("TU")(Φ_i_j)
+    market.method = "GS"
+    market.sampling_rate = 0.1
 
-# Check equilibrium
-market.check_equilibrium(u_i, v_j, mu_i_j, eps)
+    # Solve 
+    eps = 1e-3
+    scaling_factor = 0.5
+    eps_init = 10
+    tic = time.time()
+    # u_i, v_j, mu_i_j = market.forward_auction(eps= eps, return_mu_i_j=True)
+    # u_i, v_j, mu_i_j = market.reverse_auction(eps= eps, return_mu_i_j=True)
+    u_i, v_j, mu_i_j = market.forward_reverse_scaling(eps_init, eps, scaling_factor)
+    toc = time.time()
+    print(f"Time taken for auction: {toc - tic:.4f} seconds")
 
-# Check duality
-dual = u_i.sum() + v_j.sum()
-primal = (mu_i_j * Φ_i_j).sum()
-print("\n=== Duality Check ===")
-print(f"Dual value                : {dual.item():.4f}")
-print(f"Primal value (total cost) : {primal.item():.4f}")
+    # Check equilibrium
+    market.check_equilibrium(u_i, v_j, mu_i_j, eps)
+
+    # Check duality
+    dual = u_i.sum() + v_j.sum()
+    primal = (mu_i_j * Φ_i_j).sum()
+    print("\n=== Duality Check ===")
+    print(f"Dual value                : {dual.item():.4f}")
+    print(f"Primal value (total cost) : {primal.item():.4f}")
 
--- experiments/demo_convex-tax.py
+++ experiments/demo_convex-tax.py
@@ -6,33 +6,35 @@ import time
 num_i = 606
 num_j = 700
 
-torch.manual_seed(42)
-α_i_j = torch.randint(0, 5, (num_i, 1)) * torch.randint(0, 5, (1, num_j))
-γ_i_j = torch.randint(0, 5, (num_i, 1)) * torch.randint(0, 5, (1, num_j))
-t_k = torch.tensor([0, 9.701, 39.476, 84.201, 160.726, 204.101, 510.300]) / 100
-τ_k = torch.tensor([.1, .12, .22, .24, .32, .35, .37])
 
-market = get_template("convex_tax")(α_i_j, γ_i_j, t_k, τ_k)
+for _ in range(200):
+    torch.manual_seed(42)
+    α_i_j = torch.randint(0, 5, (num_i, 1)) * torch.randint(0, 5, (1, num_j))
+    γ_i_j = torch.randint(0, 5, (num_i, 1)) * torch.randint(0, 5, (1, num_j))
+    t_k = torch.tensor([0, 9.701, 39.476, 84.201, 160.726, 204.101, 510.300]) / 100
+    τ_k = torch.tensor([.1, .12, .22, .24, .32, .35, .37])
 
-# Solve 
-eps = 1e-3
-scaling_factor = 0.5
-eps_init = 10
-tic = time.time()
-# u_i, v_j, mu_i_j = market.forward_auction(eps= eps, return_mu_i_j=True)
-# u_i, v_j, mu_i_j = market.reverse_auction(eps= eps, return_mu_i_j=True)
-u_i, v_j, mu_i_j = market.forward_reverse_scaling(eps_init, eps, scaling_factor)
-toc = time.time()
-print(f"Time taken for auction: {toc - tic:.4f} seconds")
+    market = get_template("convex_tax")(α_i_j, γ_i_j, t_k, τ_k)
 
-# Check equilibrium
-market.check_equilibrium(u_i, v_j, mu_i_j, eps)
+    # Solve 
+    eps = 1e-3
+    scaling_factor = 0.5
+    eps_init = 10
+    tic = time.time()
+    # u_i, v_j, mu_i_j = market.forward_auction(eps= eps, return_mu_i_j=True)
+    # u_i, v_j, mu_i_j = market.reverse_auction(eps= eps, return_mu_i_j=True)
+    u_i, v_j, mu_i_j = market.forward_reverse_scaling(eps_init, eps, scaling_factor)
+    toc = time.time()
+    print(f"Time taken for auction: {toc - tic:.4f} seconds")
+
+    # Check equilibrium
+    market.check_equilibrium(u_i, v_j, mu_i_j, eps)
 
 # Check duality
-Φ_i_j = α_i_j + γ_i_j
-dual = u_i.sum() + v_j.sum()
-primal = (mu_i_j * Φ_i_j).sum()
-print("\n=== Duality Check ===")
-print(f"Dual value                : {dual.item():.4f}")
-print(f"Primal value (total cost) : {primal.item():.4f}")
+# Φ_i_j = α_i_j + γ_i_j
+# dual = u_i.sum() + v_j.sum()
+# primal = (mu_i_j * Φ_i_j).sum()
+# print("\n=== Duality Check ===")
+# print(f"Dual value                : {dual.item():.4f}")
+# print(f"Primal value (total cost) : {primal.item():.4f}")
 
